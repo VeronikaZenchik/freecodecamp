294b47e551436f3e97bd5f470e8d77db7081b138
VeronikaZenchik <veronikazenchik991@gmail.com>
2024-08-27 22:58:14 +0300

    task 100

--- JavaScript/Pyramid Generator/main.js
+++ JavaScript/Pyramid Generator/main.js
@@ -5,8 +5,18 @@ const rows = [];
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(padRow(i + 1, count));
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+/*while (rows.length < count) {
+  rows.push(padRow(rows.length + 1, count));
+}*/
+
+for (let i = count; i > 0; false) {
+
 }
 
 let result = ""
